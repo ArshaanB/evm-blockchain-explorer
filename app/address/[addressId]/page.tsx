f5a43132b38ce4b0e1b4ed0ba6baeb429ff2c392
Arshaan <arshaan.bhimani@uwaterloo.ca>
2023-09-18 19:38:38 -0400

    Updated readability for address/addressId pages

--- app/address/[addressId]/page.tsx
+++ app/address/[addressId]/page.tsx
@@ -2,8 +2,14 @@ import { TransactionList } from "../../components/transaction";
 
 export default function Page({ params }: { params: { addressId: string } }) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <TransactionList addressId={params.addressId} />
+    <div className="flex min-h-screen flex-col items-center  p-24">
+      <h1 className="text-4xl text-center font-bold text-gray-800">
+        Viewing transactions for address:{" "}
+        <pre className="text-gray-600">{params.addressId}</pre>
+      </h1>
+      <div className="mt-6">
+        <TransactionList addressId={params.addressId} />
+      </div>
     </div>
   );
 }

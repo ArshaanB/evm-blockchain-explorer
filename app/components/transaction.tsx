b86d1d7385eb4502d1ec621085c543ad779fbda6
Arshaan <arshaan.bhimani@uwaterloo.ca>
2023-09-19 19:04:47 -0400

    fixed display of transactionDetail made extra etherscan call for additional tx data

--- app/components/transaction.tsx
+++ app/components/transaction.tsx
@@ -84,25 +84,38 @@ interface TransactionDetail {
   from: string;
   to: string;
   value: string;
-  transactionHash: string;
+  hash: string;
   amount: string;
   blockNumber: number;
   isConfirmed: boolean;
-  transactionFee: string;
+  gas: string;
+}
+
+interface TransactionReceipt {
+  gasUsed: string;
+  status: string;
 }
 
 async function getBlockTimestamp(blockNumberHex: string, blockchain: string) {
-  console.log(blockNumberHex);
   let url = `https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=${blockNumberHex}&boolean=true&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY}`;
   if (blockchain === "Polygon") {
     url = `https://api.polygonscan.com/api?module=proxy&action=eth_getBlockByNumber&tag=${blockNumberHex}&boolean=true&apikey=${process.env.NEXT_PUBLIC_POLYGONSCAN_API_KEY}`;
   }
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
   const timestamp = parseInt(data.result.timestamp, 16);
   const date = new Date(timestamp * 1000);
   return date;
+}
+
+function getTransactionFee(gasUsed: string, gasPrice: string) {
+  const gasUsedBigInt = BigInt(gasUsed);
+  const gasPriceBigInt = BigInt(gasPrice);
+
+  const transactionFeeWei = gasUsedBigInt * gasPriceBigInt;
+  const transactionFeeEther = ethers.formatEther(transactionFeeWei);
+
+  return transactionFeeEther;
 }
 
 export const TransactionDetail = ({
@@ -110,19 +123,26 @@ export const TransactionDetail = ({
 }: TransactionDetailProps) => {
   const [transactionDetail, setTransactionDetail] =
     useState<TransactionDetail | null>(null);
+  const [transactionReceipt, setTransactionReceipt] =
+    useState<TransactionReceipt | null>(null);
   const [blockTimestamp, setBlockTimestamp] = useState<Date | null>(null);
   const { blockchain } = useContext(BlockchainContext);
 
   useEffect(() => {
     const fetchTransactionDetail = async () => {
-      let url = `https://api.etherscan.io/api?module=proxy&action=eth_getTransactionReceipt&txhash=${transactionId}&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY}`;
+      let url = `https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${transactionId}&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY}`;
+      let urlForReceipt = `https://api.etherscan.io/api?module=proxy&action=eth_getTransactionReceipt&txhash=${transactionId}&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY}`;
       if (blockchain === "Polygon") {
-        url = `https://api.polygonscan.com/api?module=transaction&action=gettxreceiptstatus&txhash=${transactionId}&apikey=${process.env.NEXT_PUBLIC_POLYGONSCAN_API_KEY}`;
+        url = `https://api.polygonscan.com/api?module=proxy&action=eth_getTransactionByHash&txhash=${transactionId}&apikey=${process.env.NEXT_PUBLIC_POLYGONSCAN_API_KEY}`;
+        urlForReceipt = `https://api.polygonscan.com/api?module=proxy&action=eth_getTransactionReceipt&txhash=${transactionId}&apikey=${process.env.NEXT_PUBLIC_POLYGONSCAN_API_KEY}`;
       }
       const response = await fetch(url);
       const data = await response.json();
       setTransactionDetail(data.result);
-      console.log(data.result);
+      const responseForReceipt = await fetch(urlForReceipt);
+      const dataForReceipt = await responseForReceipt.json();
+      setTransactionReceipt(dataForReceipt.result);
+
       // Fetch block timestamp
       const date = await getBlockTimestamp(data.result.blockNumber, blockchain);
       setBlockTimestamp(date);
@@ -135,7 +155,7 @@ export const TransactionDetail = ({
     <div className="flex flex-col items-center justify-center bg-gray-200 p-5 rounded-lg w-full overflow-auto">
       {transactionDetail && (
         <div
-          key={transactionDetail.transactionHash}
+          key={transactionDetail.hash}
           className="bg-white p-2 my-2 rounded-md shadow-md w-full overflow-auto"
         >
           <p>
@@ -145,14 +165,14 @@ export const TransactionDetail = ({
           <p>
             <span className="mb-0 font-bold">To:</span> {transactionDetail.to}
           </p>
-          {/* <p>
+          <p>
             <span className="mb-0 font-bold">Amount:</span>{" "}
             {ethers.formatEther(transactionDetail.value)}{" "}
             {blockchain === "Ethereum" ? "ETH" : "MATIC"}
-          </p> */}
+          </p>
           <p>
             <span className="mb-0 font-bold">Transaction Hash:</span>{" "}
-            {transactionDetail.transactionHash}
+            {transactionDetail.hash}
           </p>
           <p>
             <span className="mb-0 font-bold">Date:</span>{" "}
@@ -163,10 +183,15 @@ export const TransactionDetail = ({
             <span className="mb-0 font-bold">Confirmation Status:</span>{" "}
             {transactionDetail.isConfirmed ? "Confirmed" : "Pending"}
           </p>
-          <p>
+          {/* <p>
             <span className="mb-0 font-bold">Transaction Fee:</span>{" "}
-            {transactionDetail.transactionFee}
-          </p>
+            {ethers.formatEther(
+              getTransactionFee(
+                transactionReceipt.gasUsed,
+                transactionDetail.gas
+              )
+            )}
+          </p> */}
         </div>
       )}
     </div>
